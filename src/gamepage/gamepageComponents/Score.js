0672f0038d49300f4d29e0eae311a5bdbc82635c
Tim Cavey <112900862+T-J-D-Cavey@users.noreply.github.com>
2022-12-20 08:49:40 +0000

    Update Score.js
    Add className to score metric

--- src/gamepage/gamepageComponents/Score.js
+++ src/gamepage/gamepageComponents/Score.js
@@ -3,8 +3,9 @@ import { scoreSelector } from '../../redux/scoreSlice';
 
 export function Score() {
     const currentScore = useSelector(scoreSelector);
-
+    
+//     From remote: added the 'metric' className:
     return (
-        <div>{currentScore}</div>
+        <div className='metric' >{currentScore}</div>
     )
 }
